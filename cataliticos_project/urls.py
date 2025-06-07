549aa4aded552f6404a746043bcd825bb0c86d09
Mauricio Alvarado <tu_correo@ejemplo.com>
2025-06-07 02:48:24 -0400

    Fix fondo y home en producción

--- cataliticos_project/urls.py
+++ cataliticos_project/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('cataliticos.urls', namespace='cataliticos')),
+    path('', TemplateView.as_view(template_name="base.html"), name="home"),
 ]
 
 # Servir archivos multimedia en desarrollo
